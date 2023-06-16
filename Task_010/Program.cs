e6725220f2e75f59e0b7790918769107cd15dbc5
Vladislav_Tishkovskij <vladislav.sergeevich.666@mail.ru>
2023-06-17 01:06:12 +0300

    Написали программу (Task_10) 2 вариант решения задачи

--- Task_010/Program.cs
+++ Task_010/Program.cs
@@ -3,9 +3,21 @@
 // 782 -> 8
 // 918 -> 1
 
-Console.Write("Введите трёхзначное число: ");
-int number = Convert.ToInt32(Console.ReadLine());
+//Второй вариант решения данной задачи
+int Prompt(string message)
+{
+    System.Console.Write(message);
+    string value = Console.ReadLine();
+    int result = Convert.ToInt32(value);
+    return result;
+}
 
-int secondDigit = (number / 10) % 10;
-
-Console.WriteLine("Вторая цифра числа: " + secondDigit);
+int number = Prompt("Введите трехзначное число > ");
+if (number < 100 || number >= 1000)
+{
+    Console.WriteLine("Вы ввели не трехзначное число, пожалуйста повторите ввод");
+    return;
+}
+Console.WriteLine($"Введенное число '{number}'");
+int secondRank = number / 10 % 10; 
+Console.WriteLine($"Вторая цифра'{secondRank}'");

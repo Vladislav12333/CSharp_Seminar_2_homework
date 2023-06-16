a37654bd2618c4620f56da6bc43d3124edb938b6
Vladislav_Tishkovskij <vladislav.sergeevich.666@mail.ru>
2023-06-17 00:48:28 +0300

    Написали программу (Task_15) 2 вариант решения задачи

--- Task_015/Program.cs
+++ Task_015/Program.cs
@@ -3,34 +3,20 @@
 // 7 -> да
 // 1 -> нет
 
-//Первый вариант решения задачи
-Console.Write("Введите номер дня недели: ");
-int dayNumber = int.Parse(Console.ReadLine());
+//Второй вариант решения задачи
+Console.Write("Введи цифру деня недели: ");
+int dayNumber = Convert.ToInt32(Console.ReadLine());
 
-switch (dayNumber)
+void DayOfTheWeek(int dayNumber)
 {
-    case 1:
-        Console.WriteLine("Это понедельник, рабочий день.");
-        break;
-    case 2:
-        Console.WriteLine("Это вторник, рабочий день.");
-        break;
-    case 3:
-        Console.WriteLine("Это среда, рабочий день.");
-        break;
-    case 4:
-        Console.WriteLine("Это четверг, рабочий день.");
-        break;
-    case 5:
-        Console.WriteLine("Это пятница, рабочий день.");
-        break;
-    case 6:
-        Console.WriteLine("Это суббота, выходной день.");
-        break;
-    case 7:
-        Console.WriteLine("Это воскресенье, выходной день.");
-        break;
-    default:
-        Console.WriteLine("Нет такого дня недели.");
-        break;
+    if (dayNumber == 6 || dayNumber == 7)
+    {
+        Console.WriteLine("Да, этот день выходной");
+    }
+    else if (dayNumber < 1 || dayNumber > 7)
+    {
+        Console.WriteLine("Такого дня недели не существует");
+    }
+    else Console.WriteLine("Нет, этот день не выходной");
 }
+DayOfTheWeek(dayNumber);
